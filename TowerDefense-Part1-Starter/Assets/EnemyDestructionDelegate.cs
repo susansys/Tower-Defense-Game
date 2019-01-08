6601b49e9458329f45842b72d27585bae7841d55
susansys <sunsyshwh@gmail.com>
2019-01-08 18:10:33 -0500

    from tutorial raywenderlich.com

---
+++ TowerDefense-Part1-Starter/Assets/EnemyDestructionDelegate.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EnemyDestructionDelegate : MonoBehaviour
+{
+    public delegate void EnemyDelegate(GameObject enemy);
+    public EnemyDelegate enemyDelegate;
+
+    public List<GameObject> enemiesInRange;
+
+    void OnDestroy()
+    {
+        if (enemyDelegate != null)
+        {
+            enemyDelegate(gameObject);
+        }
+    }
+    // 1
+    void OnEnemyDestroy(GameObject enemy)
+    {
+        enemiesInRange.Remove(enemy);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // 2
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            enemiesInRange.Add(other.gameObject);
+            EnemyDestructionDelegate del =
+                other.gameObject.GetComponent<EnemyDestructionDelegate>();
+            del.enemyDelegate += OnEnemyDestroy;
+        }
+    }
+    // 3
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            enemiesInRange.Remove(other.gameObject);
+            EnemyDestructionDelegate del =
+                other.gameObject.GetComponent<EnemyDestructionDelegate>();
+            del.enemyDelegate -= OnEnemyDestroy;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemiesInRange = new List<GameObject>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ TowerDefense-Part1-Starter/Assets/MonsterData.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections.Generic;
+
+
+[System.Serializable]
+public class MonsterLevel
+{
+    public int cost;
+    public GameObject visualization;
+    public GameObject bullet;
+    public float fireRate;
+
+}
+
+public class MonsterData : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public List<MonsterLevel> levels;
+    private MonsterLevel currentLevel;
+    //1
+    public MonsterLevel CurrentLevel
+    {
+        //2
+        get
+        {
+            return currentLevel;
+        }
+        //3
+        set
+        {
+            currentLevel = value;
+            int currentLevelIndex = levels.IndexOf(currentLevel);
+
+            GameObject levelVisualization = levels[currentLevelIndex].visualization;
+            for (int i = 0; i < levels.Count; i++)
+            {
+                if (levelVisualization != null)
+                {
+                    if (i == currentLevelIndex)
+                    {
+                        levels[i].visualization.SetActive(true);
+                    }
+                    else
+                    {
+                        levels[i].visualization.SetActive(false);
+                    }
+                }
+            }
+        }
+    }
+    void OnEnable()
+    {
+        CurrentLevel = levels[0];
+    }
+
+    public MonsterLevel GetNextLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        int maxLevelIndex = levels.Count - 1;
+        if (currentLevelIndex < maxLevelIndex)
+        {
+            return levels[currentLevelIndex + 1];
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public void IncreaseLevel()
+    {
+        int currentLevelIndex = levels.IndexOf(currentLevel);
+        if (currentLevelIndex < levels.Count - 1)
+        {
+            CurrentLevel = levels[currentLevelIndex + 1];
+        }
+    }
+
+
+
+}
